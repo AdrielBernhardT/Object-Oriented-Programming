531546baaa04dd3f1e8f3147201717dbfe7c84f1
AdrielBernhardT <adrielbth01@gmail.com>
2025-03-14 12:20:56 +0700

    Delete library 2 dan latsoal OOP

--- Cinema UTC Exercise/src/Cinema.java
+++ Cinema UTC Exercise/src/Cinema.java
@@ -52,12 +52,12 @@ public class Cinema{
         }
         
         System.out.println("\nCustomers:");
-        for(Hall hall : halls){
-            for(Showtime showtime : hall.showtimes){
-                for(Ticket ticket : showtime.tickets){
+        for(Hall hall : halls){ // Setiap hall yang ada
+            for(Showtime showtime : hall.showtimes){ // Setiap showrime di dalam list showtime di setiap hall
+                for(Ticket ticket : showtime.tickets){ // Setiap ticket di dalam list ticket di setiap showtime 
                     System.out.println("- Name: " + ticket.customer.name + ", Age: " + ticket.customer.age);
                     System.out.println("  Tickets:");
-                    for(Ticket ticket2 : ticket.customer.tickets){
+                    for(Ticket ticket2 : ticket.customer.tickets){ // setiap ticket pada customer
                         System.out.print("    * Ticket ID: " + ticket2.id +", Movie: " + showtime.movie.title + ", Showtime: " + showtime.date + ", Date: " + showtime.date + ", Price: " + ticket.price);
                         if (ticket.paymentStatus) {
                             System.out.println(", Payment Status: Paid");
