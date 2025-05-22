cee509581eaa531882b56b6dbd5c123e6649c3c3
AdrielBernhardT <adrielbth01@gmail.com>
2025-05-22 11:07:31 +0700

    Finished Latihan UAC

--- Latihan UAC (22 Mei 2025)/src/BasicLogin.java
+++ Latihan UAC (22 Mei 2025)/src/BasicLogin.java
@@ -1,9 +1,12 @@
-
-
 public class BasicLogin implements ILogin {
     public String username;
     public String password;
     
+    public BasicLogin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     @Override
     public boolean  authenticate(String username, String password) {
         if(this.username == username && this.password == password){
@@ -11,10 +14,5 @@ public class BasicLogin implements ILogin {
         } else {
             return false;
         }
-    }
-    
-    public BasicLogin(String username, String password) {
-        this.username = username;
-        this.password = password;
     }
 }
---
+++ Latihan UAC (22 Mei 2025)/src/OAuthLogin.java
@@ -0,0 +1,15 @@
+public class OAuthLogin implements ILogin {
+    public String token;
+
+    public OAuthLogin(String token){
+        this.token = token;
+    }
+
+    @Override
+    public boolean authenticate(String username, String password) {
+        if(this.token == password){
+            return true;
+        }
+       return false;
+    }
+}
